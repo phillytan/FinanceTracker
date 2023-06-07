028d92570d70413fd5be53b9956552e1c91544cd
Phillbert Tan <philly@phillytan.xyz>
2023-06-07 08:45:12 +0800

    Add test route for React Router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./styles/theme";
@@ -8,13 +9,19 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import Test from './components/Test'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <App />
+      <BrowserRouter>
+        <App />
+        <Routes>
+          <Route path={'/test'} element={<Test />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
