1a14667a154e743c7229b2536b20be6c96b0d2f4
philly <philly@phillytan.xyz>
2023-06-07 08:15:15 +0800

    Add Material UI (#6)
    * Remove unnecessary components
    
    * Add Initial MUI Code
    
    * Add Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container } from '@mui/material'
+import NavBar from './components/NavBar';
+import TransactionsTable from './components/TransactionsTable';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<>
+			<NavBar />
+			<Container>
+        <br />
+        <TransactionsTable />
+        <br />
+				<Button variant="contained">Hello World</Button>
+			</Container>
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/TransactionsTable.js
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(
+  date, type, name, currency, amount
+) {
+  return { date, type, name, currency, amount };
+}
+
+const rows = [
+	createData("2/11/2020", "Groceries", "No Frills Vancouver", "CAD", "69.99"),
+	createData("2/12/2020", "Food", "Uncle Fatihs UBC", "CAD", "12.99"),
+];
+
+const TransactionsTable = () => {
+  return (
+		<TableContainer component={Paper}>
+			<Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+				<TableHead>
+					<TableRow>
+						<TableCell>Date</TableCell>
+						<TableCell>Type</TableCell>
+						<TableCell>Merchant Name</TableCell>
+						<TableCell align="right">Amount</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{rows.map((row) => (
+						<TableRow
+							key={row.name}
+							sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+						>
+							<TableCell component="th" scope="row">
+								{row.date}
+							</TableCell>
+							<TableCell>{row.type}</TableCell>
+							<TableCell>{row.name}</TableCell>
+							<TableCell align="right">
+								{`${row.currency} ${row.amount}`}
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	);
+}
+
+export default TransactionsTable;
