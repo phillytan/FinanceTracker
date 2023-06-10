4735524ac450ea403b4160d6724322c774406f86
Phillbert Tan <philly@phillytan.xyz>
2023-06-10 23:27:10 +0800

    chore:cleanup

--- src/components/Transactions/ViewTransactionsTable.js
+++ src/components/Transactions/ViewTransactionsTable.js
@@ -7,7 +7,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-import TransactionItem from './TransactionsItem';
+import TransactionItem from './ViewTransactionsItem';
 
 function createData(
   date, type, name, currency, amount
