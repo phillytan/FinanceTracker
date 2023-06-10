03fa5cd36b7ecce10538bc2c0fc0bf953474dba0
chin9 <64379928+chin9@users.noreply.github.com>
2023-06-10 08:12:02 -0700

    Feat/login UI (#10)
    * small changes to match design
    
    * added divider

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,28 +8,35 @@ import MenuIcon from "@mui/icons-material/Menu";
 import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
-	const navigate = useNavigate()
-	return (
-		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position="static">
-				<Toolbar>
-					<IconButton
-						size="large"
-						edge="start"
-						color="inherit"
-						aria-label="menu"
-						sx={{ mr: 2 }}
-					>
-						<MenuIcon />
-					</IconButton>
-					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-						Finance Tracker
-					</Typography>
-					<Button color="inherit" onClick={() => navigate("/login")}>Login</Button>
-				</Toolbar>
-			</AppBar>
-		</Box>
-	);
+  const navigate = useNavigate();
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, cursor: "pointer" }}
+            onClick={() => navigate("/")}
+          >
+            Finance Tracker
+          </Typography>
+          <Button color="inherit" onClick={() => navigate("/login")}>
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
 
 export default NavBar;
