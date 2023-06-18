f94d3d77c85d6068b6d5234edd8f077aaa9d85ab
vangeliq <valeryangelique2001@gmail.com>
2023-06-18 08:53:54 -0700

    refactored bar chart & moved it to dashboard
    refactored bar chart so it has its own component and moved it to the dashboard.

--- src/pages/DashBoardPage.js
+++ src/pages/DashBoardPage.js
@@ -1,14 +1,65 @@
 import * as React from "react";
 import Container from "@mui/material/Container";
+import DashboardBarChart from "../components/DashboardGraphs/BarChart";
 
 /**
  * Component for the dashboard page
  */
 export default function DashBoardPage() {
+  const sampleData = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
 
   return (
-    <Container component="main" maxWidth="xs">
-        This is the Dashboard
+    <Container
+      component="main"
+      maxWidth="xs"
+      sx={{
+        paddingTop: "15px",
+      }}
+    >
+      <DashboardBarChart title="Title Here" data={sampleData} />
     </Container>
   );
 }
--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,52 +1,7 @@
-import { Button, Container, Card, CardContent, Typography } from "@mui/material";
+import { Container } from "@mui/material";
 import ViewTransactionsTable from "../components/Transactions/ViewTransactionsTable";
 import AddTransactionItem from "../components/Transactions/AddTransactionItem";
-import { BarChart, Bar, ResponsiveContainer, XAxis, YAxis, Label } from 'recharts';
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
 
 function IndexPage() {
 	return (
@@ -58,19 +13,6 @@ function IndexPage() {
 				</div>
 				<ViewTransactionsTable />
 				<br />
-        <Card variant={'outlined'}>
-          <CardContent>
-            <Typography variant={'h6'} align={'center'}>Chart Name</Typography>
-            <ResponsiveContainer width={"100%"} height={256}>
-              <BarChart title="Chart Name" width={150} height={40} data={data}>
-                <XAxis dataKey={'index'} />
-                <YAxis type={'number'} />
-                <Bar dataKey="uv" fill="#8884d8" />
-              </BarChart>
-            </ResponsiveContainer>
-          </CardContent>
-        </Card>
-        <br />
 			</Container>
 		</>
 	);
