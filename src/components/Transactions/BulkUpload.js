73c6420004176669fd8f210c26a90e1ccc289de8
Chinchin Jin <chinchinjin@gmail.com>
2023-07-03 15:37:22 -0700

    handle no file case

--- src/components/Transactions/BulkUpload.js
+++ src/components/Transactions/BulkUpload.js
@@ -9,18 +9,24 @@ export default function BulkUpload() {
     console.log(transactions);
   }, [transactions]);
 
-  const handleUpload = async (event) => {
-    Papa.parse(event.target.files[0], {
-      header: true,
-      skipEmptyLines: true,
-      complete: (results) => {
-        setTransactions(results.data);
-      },
-    });
+  const handleUpload = (event) => {
+    const file = event.target.files[0]
+    if (file) {
+      Papa.parse(file, {
+        header: true,
+        skipEmptyLines: true,
+        complete: (results) => {
+          setTransactions(results.data);
+        },
+        error: (error) => {
+            console.log(error)
+        }
+      });
+    }
   };
 
   return (
-    <div style={{ marginLeft: '5px'}}>
+    <div style={{ marginLeft: "5px" }}>
       <Button variant="outlined" component="label">
         Bulk upload
         <input type="file" hidden accept=".csv" onChange={handleUpload} />
