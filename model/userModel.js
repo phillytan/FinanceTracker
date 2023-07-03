107f86626410b46560da0d9752bfbb83bc9cd147
Personal <rliu8912@gmail.com>
2023-07-03 16:52:55 -0700

    setup mongodb, create schemas + dbconn

---
+++ model/userModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const { encrypt, decrypt } = require("../util/security");
+
+const Schema = mongoose.Schema;
+
+const User = new Schema(
+  {
+    fname: { type: String, required: true },
+    lname: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    kusername: { type: String, required: true },
+    transactions: [{ type: Schema.Types.ObjectId, ref: "transaction" }],
+  },
+  { timestamps: true, toObject: { getters: true }, toJSON: { getters: true } }
+);
+module.exports = mongoose.model("users", User);
