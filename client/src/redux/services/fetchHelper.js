ca1d31a06b47fa9dfe49cea8766dace46c2664e8
Phillbert Tan <philly@phillytan.xyz>
2023-07-19 08:24:41 +0800

    Bug Fixes

--- client/src/redux/services/fetchHelper.js
+++ client/src/redux/services/fetchHelper.js
@@ -1,24 +1,25 @@
-const ENDPOINT = "http://localhost:3001";
+const ENDPOINT = "https://financetracker-backend.onrender.com";
+// const ENDPOINT = "http://localhost:3001";
 
 const fetchHelper = async (path, method, body) => {
-  const accesstoken = localStorage.getItem("accesstoken")
+	const accesstoken = localStorage.getItem("accesstoken")
 		? localStorage.getItem("accesstoken")
 		: null;
-  const response = await fetch(ENDPOINT + path, {
-    method,
-    headers: {
-      "Content-Type": "application/json",
-      accesstoken
-    },
-    [method.toUpperCase() !== 'GET' ? 'body' : undefined]: JSON.stringify(body),
-  });
-  const data = await response.json();
-  console.log(data)
-  if (!response.ok) {
-    global.setNotification('error', data.message)
-    throw new Error(data.message)
-  }
-  return data
+	const response = await fetch(ENDPOINT + path, {
+		method,
+		headers: {
+			"Content-Type": "application/json",
+			accesstoken,
+		},
+		[method.toUpperCase() !== "GET" ? "body" : undefined]: JSON.stringify(body),
+	});
+	const data = await response.json();
+	console.log(data);
+	if (!response.ok) {
+		global.setNotification("error", data.message);
+		throw new Error(data.message);
+	}
+	return data;
 };
 
-module.exports = fetchHelper;
+export default fetchHelper;
