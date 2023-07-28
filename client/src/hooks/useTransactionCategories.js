9d1ab55c89520db0a39e2e320e0182943c9df863
Chinchin Jin <chinchinjin@gmail.com>
2023-07-27 23:18:23 -0700

    replace dummy data in dashboard

---
+++ client/src/hooks/useTransactionCategories.js
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+import { roundToTwoDecimals } from "../utils/math";
+import { getExchangeRates } from "../utils/currency";
+
+const useTransactionCategories = (transactions) => {
+  const [recentTransactions, setRecentTransactions] = useState([]);
+  const [categoriesData, setCategoriesData] = useState([]);
+  const [exchangeRateMap, setExchangeRateMap] = useState({});
+
+  useEffect(() => {
+    const currentDate = new Date();
+    const oneMonthAgo = new Date(currentDate);
+    oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
+    const recent = transactions.filter((transaction) => {
+      const transactionDate = new Date(transaction.date);
+      return oneMonthAgo <= transactionDate && transactionDate <= currentDate;
+    });
+    setRecentTransactions(recent);
+  }, [transactions]);
+
+  useEffect(() => {
+    getExchangeRates(recentTransactions).then((res) => setExchangeRateMap(res));
+  }, [recentTransactions]);
+
+  useEffect(() => {
+    const getTransactionsCAD = async () =>
+      await Promise.all(
+        recentTransactions.map(async (transaction) => {
+          return {
+            currency: "CAD",
+            transactionType: transaction.transactionType,
+            amount: roundToTwoDecimals(
+              exchangeRateMap[transaction.currency] * Number(transaction.amount)
+            ),
+            date: transaction.date,
+          };
+        })
+      );
+
+    getTransactionsCAD().then((res) => {
+      const pieData = res.reduce((acc, transaction) => {
+        const { amount, transactionType } = transaction;
+        const categoryExists = acc.find(
+          (item) => item.name === transactionType
+        );
+        if (categoryExists) {
+          categoryExists.value += amount;
+        } else {
+          acc.push({ name: transactionType, value: amount });
+        }
+        return acc;
+      }, []);
+
+      setCategoriesData(
+        pieData.map((category) => ({
+          name: category.name,
+          value: roundToTwoDecimals(category.value),
+        }))
+      );
+    });
+  }, [recentTransactions, exchangeRateMap]);
+
+  return { categoriesData };
+};
+
+export { useTransactionCategories };
--- client/src/utils/date.js
+++ client/src/utils/date.js
@@ -1,3 +1,28 @@
 export const getDateString = (date) => {
     return new Date(date).toISOString().split('T')[0]
 }
+
+export const getDatesOfPastMonth = () => {
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth() + 1;
+    const currentYear = currentDate.getFullYear();
+    const currentDay = currentDate.getDay()
+
+    const startDate = new Date(currentYear, currentMonth - 1, currentDay);
+  
+    const endDate = new Date(currentYear, currentMonth, currentDay);
+  
+    const datesList = [];
+  
+    let currentDateIterator = new Date(startDate);
+    while (currentDateIterator <= endDate) {
+        const year = currentDateIterator.getFullYear();
+        const month = String(currentDateIterator.getMonth() + 1).padStart(2, '0');
+        const day = String(currentDateIterator.getDate()).padStart(2, '0');
+        const formattedDate = `${year}-${month}-${day}`;
+        datesList.push(formattedDate);
+        currentDateIterator.setDate(currentDateIterator.getDate() + 1);
+    }
+  
+    return datesList;
+  };
