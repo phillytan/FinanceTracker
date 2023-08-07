d61acd0cf7e863456584d85ceaba5d6a708b1d5a
Chinchin Jin <64379928+chin9@users.noreply.github.com>
2023-08-06 17:30:14 -0700

    Feat/bulk upload data mapping (#40)
    * basic function
    
    * form validation and error messages
    
    * accidentally deleted package
    
    * remove unnecessary console log

---
+++ client/src/components/Transactions/FieldSelect.js
@@ -0,0 +1,50 @@
+// Reference: https://stackoverflow.com/questions/63236951/how-to-use-material-ui-select-with-react-hook-form
+import React from "react";
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  FormHelperText,
+} from "@mui/material";
+import { Controller } from "react-hook-form";
+
+const FieldSelect = ({ name, label, control, children, fields, ...props }) => {
+  const labelId = `${name}-label`;
+  return (
+    <FormControl {...props}>
+      <InputLabel id={labelId}>
+        {label}
+      </InputLabel>
+      <Controller
+        id={label}
+        control={control}
+        name={label}
+        render={({ field, fieldState }) => (
+          <>
+            <Select
+              label={label}
+              id={labelId}
+              select
+              {...field}
+              {...fieldState}
+            >
+              <MenuItem key={`${label}-none`} value="">
+                None
+              </MenuItem>
+              {fields.map((field) => (
+                <MenuItem key={`${label}-${field}`} value={field}>
+                  {field}
+                </MenuItem>
+              ))}
+            </Select>
+            {fieldState.error && (
+              <FormHelperText error>{fieldState.error.message}</FormHelperText>
+            )}
+          </>
+        )}
+      />
+    </FormControl>
+  );
+};
+export default FieldSelect;
